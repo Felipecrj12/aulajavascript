e5ab1900dbcdb3405d8d26257019a98d3a047013
Felipe Santos <79484864+Felipecrj12@users.noreply.github.com>
2021-05-15 17:59:30 -0300

    Add files via upload

---
+++ aula javaScript if for/script.js
@@ -0,0 +1,115 @@
+//  ||| EXERCICIO 1||| 
+
+//var lista = ["maça","pêra", "laranja"];
+//console.log(lista);
+
+
+//||| EXERCICIO 2||| 
+
+//*var lista = ["maça", "pêra", "laranja"];
+//console.log(lista[1]);
+//alert(lista[1]);*/
+
+
+// ||| EXERCICIO 3||| 
+
+
+
+//var lista = ["maça","pêra", "laranja"];
+//lista.push("uva")
+//console.log(lista);
+
+
+
+
+//||| EXERCICIO 4|||  
+
+
+
+
+//var lista = ["maça","pêra","laranja"];
+//lista.push("uva") PARA COLOCA O ELEMENTO NA LISTA 
+
+//lista.pop() PARA TIRA O ELEMENTO DA LISTA 
+
+//console.log(lista.length); PARA MOSTRAR QUANTIDADE ELEMENTOS QUE TEM NA MINHA LISTA.
+
+
+
+
+//||| EXERCICIO 5|||  
+
+
+// var lista = ["maça","pêra", "laranja"];
+//lista.push("uva")
+
+//console.log(lista.reverse()); ASSIM CONSIGO TRAZER OS ELEMENTO AO CONTRARIO NA LISTA
+
+
+
+//||| EXERCICIO 6|||
+
+
+//var lista = ["maça","pêra", "laranja"];
+//lista.push("uva")
+
+//console.log(lista.toString()); ELE VAI TRAZER PARA FORMA DE STRING
+
+//var lista = ["maça","pêra", "laranja"];
+//lista.push("uva")
+
+
+//console.log(lista[0])
+//console.log(lista.toString()[0]);
+
+
+
+//||| EXERCICIO 7|||
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva")
+
+//console.log(lista.join(" - "));
+
+//||| EXERCICIO 8 |||
+
+//var fruta = { nome: "maça", cor: "vermelha" }
+//console.log(fruta);
+
+//var fruta = { nome: "maça", cor: "vermelha" }
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+//||| EXERCICIO 9 |||
+
+//var frutas = [{ nome: "maça", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+
+
+//||| EXERCICIO 10 |||
+
+
+/*
+var idade = prompt("Qual sua idade");
+//var idade = 18;
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade");
+};
+
+*/
+
+//||| EXERCICIO 11 ||| Usando FOR estrutura de repetição
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+}
+
+*/
